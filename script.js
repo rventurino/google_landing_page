766cb90315766e3cf541a069e725a1c5460cf95c
Ryan Venturino <adventurino@outlook.com>
2021-06-26 12:27:36 -0400

    acc modal plan/resources

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 /* TODO
 
-Code modals:
-    a.) google apps - get 20-30 i tag images and organize with css grid
-    style the scroll bar
-    b.) modal for accounts tab, should be straightforward with flex
-    c.) The customize menu is gonna be the real deal here, lots going on
-    page must be customizable and use local storage to save selected settings
 
 
     google apps modal
