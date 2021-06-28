50e8b400a115900ab4237ff2799b56d8adac0644
Ryan Venturino <adventurino@outlook.com>
2021-06-28 12:27:17 -0400

    toggle apps modal

--- script.js
+++ script.js
@@ -4,3 +4,23 @@
 
     google apps modal
 */
+
+const appsModalButton = document.getElementById("google-apps");
+const appsModal = document.getElementById("apps-modal-container");
+const main = document.getElementsByTagName('main');
+
+
+//toggle apps modal
+appsModalButton.addEventListener('click', (e) => {
+    appsModal.classList.toggle("show")
+
+})
+
+window.onclick = () => {
+    appsModal.classList.remove("show")
+}
+
+appsModalButton.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+})
