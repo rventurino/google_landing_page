26d77b0e0573b663e3cabf4bdd469e4efa807a99
Ryan Venturino <adventurino@outlook.com>
2021-06-28 15:10:33 -0400

    account modal

--- script.js
+++ script.js
@@ -7,20 +7,39 @@
 
 const appsModalButton = document.getElementById("google-apps");
 const appsModal = document.getElementById("apps-modal-container");
-const main = document.getElementsByTagName('main');
+const accountModalButton = document.getElementById("google-account");
+const accountModal = document.getElementById("google-accounts-modal");
 
 
 //toggle apps modal
 appsModalButton.addEventListener('click', (e) => {
     appsModal.classList.toggle("show")
-
+    accountModal.classList.remove("show")
 })
 
 window.onclick = () => {
     appsModal.classList.remove("show")
 }
 
-appsModalButton.addEventListener('click', (e) => {
+
+//toggle account modal
+accountModalButton.addEventListener('click', (e) => {
+    accountModal.classList.toggle("show")
+    appsModal.classList.remove("show")
+})
+
+window.onclick = () => {
+    accountModal.classList.remove("show")
+    appsModal.classList.remove("show")
+}
+
+
+/* stop event loop propagation */
+accountModalButton.addEventListener('click', (e) => {
     e.stopPropagation();
 
+})
+
+appsModalButton.addEventListener('click', (e) => {
+    e.stopPropagation();
 })
