0283a5bf1b73319c013dad1340ba9d068c8c1722
Ryan Venturino <adventurino@outlook.com>
2021-07-02 15:25:40 -0400

    backgrounds logo styler

--- script.js
+++ script.js
@@ -28,32 +28,30 @@ const bgSix = document.getElementById("six")
 const bgSeven = document.getElementById("seven")
 const bgEight = document.getElementById("eight")
 
+/*background images*/
 const imgOne = "url(img/backgrounds/annie-spratt-3tlHKNAv1eE-unsplash.jpg)";
 const imgTwo = "url(img/backgrounds/claudio-schwarz-purzlbaum-98hDglL83q0-unsplash.jpg)";
 const imgThree = "url(img/backgrounds/dustin-humes-P326A4p7aeE-unsplash.jpg)";
-const imgFour = "url(img/backgrounds/julien-riedel-OH_MxLlYYSg-unsplash.jpg)";
+const imgFour = "url(img/backgrounds/hermansyah-7uXn7nudorc-unsplash.jpg)";
 const imgFive = "url(img/backgrounds/levi-stute-OhZd1t1Fm9M-unsplash.jpg)";
 const imgSix = "url(img/backgrounds/marek-piwnicki-gA2i_h4XRJU-unsplash.jpg)";
 const imgSeven = "url(img/backgrounds/silvan-schuppisser-PGU_XpT47iw-unsplash.jpg)";
 const imgEight = "url(img/backgrounds/yang-shuo-oYprTx37G9g-unsplash.jpg)";
 
+/*important elements for style change */
+const header = document.getElementById("header");
+const circleClass = document.getElementsByClassName("circle");
 const googleLogo = document.getElementById("logo-inner");
+const addShortcutSpan = document.getElementById("addShortcutSpan")
+
+/*background image selector*/
+let tempBackground = null;
+let newBG = false;
+let textColor = 0; // 0: default 1: white 2: dark
+let currentBackground = localStorage.getItem("currentBackground");
 
 window.onload = () => {
-    document.body.style.backgroundImage = localStorage.getItem("currentBackground")
-    
-    for(let i = 0; i < 6; i++){
-        /*
-        googleLogo.children[i].classList.add("default")
-        googleLogo.children[i].classList.style.color = "white"
-        googleLogo.children[i].classList.style.color = "#333"
-        make boolean that says if theme should be light, dark, or default
-        should be inside if statement for newBG I think
-        */
-        
-    }
-    
-    console.log(googleLogo.childNodes)
+    document.body.style.backgroundImage = currentBackground;
 }
 
 //toggle apps modal
@@ -135,67 +133,109 @@ colorsButton.addEventListener('click', (e) => {
     colorsMenu.classList.add("show");
 })
 
-/*background image selector*/
-let tempBackground = null;
-let newBG = false;
+
+/* BACKGROUND CHOICES */
 
 /* one */
 bgOne.addEventListener('click', () => {
     document.body.style.backgroundImage = imgOne;
     tempBackground = imgOne;
     newBG = true;
-    console.log(tempBackground)
+    textColor = 2;
 })
 /* two */
 bgTwo.addEventListener('click', () => {
     document.body.style.backgroundImage = imgTwo;
     newBG = true;
     tempBackground = imgTwo;
+    textColor = 1;
 })
 /* three */
 bgThree.addEventListener('click', () => {
     document.body.style.backgroundImage = imgThree;
     newBG = true;
     tempBackground = imgThree;
+    textColor = 1;
 })
 /* four */
 bgFour.addEventListener('click', () => {
     document.body.style.backgroundImage = imgFour;
     newBG = true;
     tempBackground = imgFour;
+    textColor = 2;
 })
 /* five */
 bgFive.addEventListener('click', () => {
     document.body.style.backgroundImage = imgFive;
     newBG = true;
     tempBackground = imgFive;
+    textColor = 1;
 })
 /* six */
 bgSix.addEventListener('click', () => {
     document.body.style.backgroundImage = imgSix;
     newBG = true;
     tempBackground = imgSix;
+    textColor = 1;
 })
 /* seven */
 bgSeven.addEventListener('click', () => {
     document.body.style.backgroundImage = imgSeven;
     newBG = true;
     tempBackground = imgSeven;
+    textColor = 2;
 })
 /* eight */
 bgEight.addEventListener('click', () => {
     document.body.style.backgroundImage = imgEight;
     newBG = true;
     tempBackground = imgEight;
+    textColor = 2;
 })
-
+/*
+header
+.circle
+#addShortcutSpan
+.logo-inner children
+*/
+// 0: default 1: white 2: dark
 /* Save state set in customizer */
 customizerDone.addEventListener('click', () => {
     customUnderlay.classList.remove("show");
     if(newBG){
         localStorage.setItem("currentBackground", tempBackground);
+        colorChanger(textColor)
     }
+    
 })
+
+colorChanger = (colorNumber) =>{
+    switch(colorNumber){
+        case 0: 
+            header.style.color = "#333";
+            addShortcutSpan.style.color = "#333"
+            for(let i = 0; i < circleClass.length; i++){
+                circleClass[i].style.backgroundColor = "#333"
+            }
+            break;
+        case 1: 
+            header.style.color = "white";
+            googleLogo.style.color = "white";
+            addShortcutSpan.style.color = "white";
+            for(let i = 0; i < circleClass.length; i++){
+                circleClass[i].style.backgroundColor = "#333"
+            }
+            break;
+        case 2: 
+            header.style.color = "#333";
+            googleLogo.style.color = "#333";
+            addShortcutSpan.style.color ="#333"
+            for(let i = 0; i < circleClass.length; i++){
+                circleClass[i].style.backgroundColor = "#333"
+            }
+            break;
+    }
+}
 
 /* color menu hover effects */
 
